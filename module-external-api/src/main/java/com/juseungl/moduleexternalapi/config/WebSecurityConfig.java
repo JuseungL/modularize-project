38492ab6f24292dac2f44246377cc6ee07686761
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-09-14 21:31:16 +0900

    REFACTOR : ExceptionHandling & SecurityConfig 2

--- module-external-api/src/main/java/com/juseungl/moduleexternalapi/config/WebSecurityConfig.java
+++ module-external-api/src/main/java/com/juseungl/moduleexternalapi/config/WebSecurityConfig.java
@@ -19,11 +19,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
