f061a4b425e52a308081413b5359190f3123d52e
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-09-18 02:14:54 +0900

    FEAT : spring batch data configuration

--- module-batch/src/main/java/com/juseungl/modulebatch/config/BatchDataSourceConfig.java
+++ module-batch/src/main/java/com/juseungl/modulebatch/config/BatchDataSourceConfig.java
@@ -5,14 +5,18 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
+/**
+ * BatchDataSourceConfig 클래스는 Spring Boot 애플리케이션에서 두 개의 데이터소스 빈을 정의하는 설정 클래스
+ * - 하나는 배치 작업에 사용될 데이터소스
+ * - 다른 하나는 주 애플리케이션(비즈니스 로직)에 사용될 데이터소스
+ *
+ * DataSourceBuilder를 사용하여 데이터소스를 생성 후, 설정 프리픽스를 사용하여 데이터소스 속성을 로드
+ */
 @Configuration
 public class BatchDataSourceConfig {
-
     @Bean(name = "batchDataSource")
     @ConfigurationProperties(prefix = "spring.datasource-meta")
     public DataSource batchDataSource() {
