f2210840e4f1260458a97581c99490f882c0b93c
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-09-17 20:09:57 +0900

    FEAT : 스프링 배치 처리 1

--- module-external-api/src/main/java/com/juseungl/moduleexternalapi/oauth/exception/AuthException.java
+++ module-external-api/src/main/java/com/juseungl/moduleexternalapi/oauth/exception/AuthException.java
@@ -2,8 +2,6 @@ package com.juseungl.moduleexternalapi.oauth.exception;
 
 import com.juseungl.modulecommon.exception.BaseErrorCode;
 import com.juseungl.modulecommon.exception.CustomException;
-import com.juseungl.modulecommon.response.ApiResponse;
-import org.springframework.http.ResponseEntity;
 
 public class AuthException extends CustomException {
     public AuthException(BaseErrorCode baseErrorCode) {
